e11dfdc706f3aa19eb484ac209e38a1db99461c3
jhp2060 <jhp2060@gmail.com>
2020-07-11 01:59:59 +0900

    feat(service): Notice service

---
+++ src/main/java/com/ddingdongsogang/backend/springboot/service/notice/NoticeService.java
@@ -0,0 +1,46 @@
+package com.ddingdongsogang.backend.springboot.service.notice;
+
+import com.ddingdongsogang.backend.springboot.domain.notice.Notice;
+import com.ddingdongsogang.backend.springboot.domain.notice.NoticeRepository;
+import com.ddingdongsogang.backend.springboot.web.dto.NoticeResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class NoticeService {
+    private final NoticeRepository noticeRepository;
+
+    public List<NoticeResponseDto> findAll() {
+        List<Notice> entities = noticeRepository.findAll();
+        List<NoticeResponseDto> ret = new ArrayList<NoticeResponseDto>();
+        for (Notice n : entities){
+            ret.add(new NoticeResponseDto(n));
+        }
+        return ret;
+    }
+
+    public List<NoticeResponseDto> findByBoardId(Long boardId) {
+        List <Notice> entities = noticeRepository.findAll();
+        List<NoticeResponseDto> ret = new ArrayList<NoticeResponseDto>();
+        entities.stream().filter(
+                e->e.getBoard().getId().equals(boardId))
+                .collect(Collectors.toList());
+        for (Notice n : entities){
+            ret.add(new NoticeResponseDto(n));
+        }
+        return ret;
+    }
+
+    public NoticeResponseDto findById(Long id) {
+        Notice entity = noticeRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException(
+                        "No notices with that id (id: "+ id+")")
+        );
+        return new NoticeResponseDto(entity);
+    }
+}
