a12cc25d343d5efcbb68badf2c7a779d5ad1eaa2
jhp2060 <jhp2060@gmail.com>
2020-07-12 01:15:52 +0900

    feat(service): Site service

---
+++ src/main/java/com/ddingdongsogang/backend/springboot/service/site/SiteService.java
@@ -0,0 +1,26 @@
+package com.ddingdongsogang.backend.springboot.service.site;
+
+import com.ddingdongsogang.backend.springboot.domain.site.Site;
+import com.ddingdongsogang.backend.springboot.domain.site.SiteRepository;
+import com.ddingdongsogang.backend.springboot.web.dto.SiteResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class SiteService {
+
+    SiteRepository siteRepository;
+
+    public List<SiteResponseDto> findAll() {
+        List<Site> entities = siteRepository.findAll();
+        List<SiteResponseDto> ret = new ArrayList<>();
+        for (Site s : entities){
+            ret.add(new SiteResponseDto(s));
+        }
+        return ret;
+    }
+}
