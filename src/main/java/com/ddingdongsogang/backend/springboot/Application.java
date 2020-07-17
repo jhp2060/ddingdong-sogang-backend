fb8242fc9e09c9aada11f13582a128b536595ea0
jhp2060 <jhp2060@gmail.com>
2020-07-17 23:59:10 +0900

    feat(crawl): testing new crawl for "saint 일반공지"

--- src/main/java/com/ddingdongsogang/backend/springboot/Application.java
+++ src/main/java/com/ddingdongsogang/backend/springboot/Application.java
@@ -1,5 +1,6 @@
 package com.ddingdongsogang.backend.springboot;
 
+import com.ddingdongsogang.backend.springboot.web.BoardCrawler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -7,8 +8,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         // spring boot 내장 WAS를 실행. Tomcat이 없어도 서비스 배포 및 실행 가능
-        SpringApplication.run(Application.class, args);
+//        SpringApplication.run(Application.class, args);
+        BoardCrawler boardCrawler = new BoardCrawler();
+        boardCrawler.getNoticeTitle(1L);
     }
 }
--- src/main/java/com/ddingdongsogang/backend/springboot/web/BoardCrawler.java
+++ src/main/java/com/ddingdongsogang/backend/springboot/web/BoardCrawler.java
@@ -1,5 +1,49 @@
 package com.ddingdongsogang.backend.springboot.web;
 
-public class BoardCrawler {
+import com.ddingdongsogang.backend.springboot.domain.board.Board;
+import com.ddingdongsogang.backend.springboot.domain.board.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.jni.Local;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+//@RequiredArgsConstructor
+public class BoardCrawler {
+//    private final BoardRepository boardRepository;
+
+    public String getNoticeTitle(Long id) throws IOException {
+        /*
+        Board board = boardRepository.findById(id).orElse(null);
+        if (board == null) {
+            System.out.println("You've got the wrong id for the board.");
+            return null;
+        }
+        String url = board.getUrl();
+        System.out.println("This is the board of " + board.getName());
+        */
+        String url = "http://www.sogang.ac.kr/front/boardlist.do?bbsConfigFK=1";
+        Document doc = Jsoup.connect(url).get();
+        Elements boardElements = doc.select("div.subject");
+        for (Element e : boardElements) {
+            String noticeUrl = "http://www.sogang.ac.kr" + e.getElementsByTag("a").first().attr("href");
+            doc = Jsoup.connect(noticeUrl).get();
+            Elements noticeElements = doc.select("span.text");
+            String title = noticeElements.first().text();
+            noticeElements = doc.select("div.unit");
+            String author = noticeElements.get(0).select("span.value").text();
+            System.out.println(noticeElements.get(0).select("span.value").text());
+            LocalDateTime postedAt = LocalDateTime.parse(
+                    noticeElements.get(1).select("span.value").text(),
+                    DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss"));
+            System.out.println(title + " " + author + " "+ postedAt);
+
+        }
+        return doc.toString();
+    }
 }
