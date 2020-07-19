4ede5034e8f0bd7de13b69fd3b2ca116b86ba221
jhp2060 <jhp2060@gmail.com>
2020-07-20 00:55:22 +0900

    feat(domain): new jpa method on Board : findBySite

--- src/main/java/com/ddingdongsogang/backend/springboot/domain/board/BoardRepository.java
+++ src/main/java/com/ddingdongsogang/backend/springboot/domain/board/BoardRepository.java
@@ -1,9 +1,11 @@
 package com.ddingdongsogang.backend.springboot.domain.board;
 
+import com.ddingdongsogang.backend.springboot.domain.site.Site;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
     public List<Board> findByNameContains(String name);
+    public List<Board> findBySite(Site site);
 }
