fbb8b3553055da3b710fea40bb2ddc9032facf07
jhp2060 <jhp2060@gmail.com>
2020-07-19 23:59:31 +0900

    feat(crawl): when getting new notices, move on to the next page

--- src/main/java/com/ddingdongsogang/backend/springboot/web/BoardCrawler.java
+++ src/main/java/com/ddingdongsogang/backend/springboot/web/BoardCrawler.java
@@ -2,6 +2,7 @@ package com.ddingdongsogang.backend.springboot.web;
 
 import com.ddingdongsogang.backend.springboot.domain.board.Board;
 import com.ddingdongsogang.backend.springboot.domain.board.BoardRepository;
+import com.ddingdongsogang.backend.springboot.domain.site.Site;
 import lombok.RequiredArgsConstructor;
 import org.apache.tomcat.jni.Local;
 import org.jsoup.Jsoup;
@@ -43,15 +44,58 @@ public class BoardCrawler {
         }
     }
 
-    public void saveNoticesOfBoard(Long id) throws IOException {
-        Board board = boardRepository.findById(id).orElse(null);
+    public void saveNoticesOfBoard(Long boardId) throws IOException {
+        Board board = boardRepository.findById(boardId).orElse(null);
         if (board == null) {
             System.out.println("You've got the wrong id for the board.");
             return;
         }
-        String url = board.getUrl();
         System.out.println("This is the board of " + board.getName());
-        Document doc = Jsoup.connect(url).get();
 
+        String siteName = board.getSite().getName();
+
+        if (siteName.equals("세인트")) {
+            saveNoticesOfSaintSite(board);
+        }
+        else if (siteName.equals("컴퓨터공학과")) {
+
+        }
+    }
+
+    public void saveNoticesOfSaintSite(Board board) throws IOException {
+        Site site = board.getSite();
+        int page = 1;
+        Long latestActualNoticeId = 0L;
+        while (true) {
+            String boardUrl = board.getUrl() + "&currentPage=" + page;
+            Document doc = Jsoup.connect(boardUrl).get();
+            Elements boardElements = doc.select("div.subject");
+            for (Element e : boardElements) {
+                String noticeUrl = board.getSite().getUrl() + e.getElementsByTag("a").first().attr("href");
+                doc = Jsoup.connect(noticeUrl).get();
+
+                Long actualNoticeId = Long.parseLong(
+                        noticeUrl.substring(noticeUrl.indexOf("?pkid=") + 6, noticeUrl.indexOf("&"))
+                );
+
+                if (latestActualNoticeId < actualNoticeId) latestActualNoticeId = actualNoticeId;
+
+                System.out.print(actualNoticeId + ": ");
+
+                Elements noticeElements = doc.select("span.text");
+                String title = noticeElements.first().text();
+                noticeElements = doc.select("div.unit");
+                String author = noticeElements.get(0).select("span.value").text();
+                LocalDateTime postedAt = LocalDateTime.parse(
+                        noticeElements.get(1).select("span.value").text(),
+                        DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss"));
+
+                System.out.println(title + " / " + author + " / "+ postedAt);
+            }
+            if (board.getLatestNoticeId() < latestActualNoticeId) ++page;
+            else break;
+        }
+        board.setLatestNoticeId(latestActualNoticeId);
+        this.boardRepository.save(board);
     }
 }
